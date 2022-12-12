7a9daced6dd670080718549138ed6ffb04673f67
SWUTM <balcknutfdsfsddfsdsf@gmail.com>
2022-12-12 09:11:04 +0200

    pw4_1

--- PW4.cpp
+++ PW4.cpp
@@ -1,90 +1,21 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-extern void Check_user();
-
-
-struct Special_Date {
-	string welcome;
-	int number;
-	int time;
-	int ID;
-
-
-};
-
-struct User_login_in
-{
-	void Check_user(string User_password, string User_login)
-	{
-		string login = "kazmur";
-		string password = "12345678";
-
-		cout << "Enter login: ";
-		cin >> User_login;
-
-		cout << "Enter password: ";
-		cin >> User_password;
-
-		if (User_login == login && User_password == password)
-		{
-			cout << "You entered!" << endl;
-		}
-		else
-		{
-			cout << "Error" << endl;
-
-			exit(0);
-		}
-	};
-
-};
-
-
-
-struct TwoFA
-{
-	string TwoFAg;
-	int special_code;
-};
-
-
-
-
-
 int main()
 {
+    //10
+    int x;
+    int y = 3;
 
-	Special_Date sd;
-	sd.welcome = "Welcome";
-	sd.number = 5454;
-	sd.time = 5;
-	sd.ID = 547589230;
-	cout << sd.welcome << endl;
-	cout << "Number: " << sd.number << endl;
-	cout << "Time: " << sd.time << endl;
-	cout << "ID: " << sd.ID << endl;
+    cout << "Enter number x: ";
+    cin >> x;
+    // індуктивна та дедуктивна
+    if (x * y < 15) {
+        cout << "Condition is fulfilled" << endl;
+    }
 
-
-
-	User_login_in uli;
-	string login;
-	string password;
-	uli.Check_user(login, password);
-
-	TwoFA tfa;
-	tfa.TwoFAg = "2FA";
-	tfa.special_code;
-	cout << tfa.TwoFAg << endl;
-
-	cout << "Enter Special Code: ";
-	cin >> tfa.special_code;
-
-
-	system("pause");
-	return 0;
-
-
+    if (x * y >= 15) {
+        cout << "Condition is not fulfilled" << endl;
+    }
 }
